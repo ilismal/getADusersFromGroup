e536a0109c6d100080a8f5b725b685401f2a9966
Ignacio Lis <ilismal@users.noreply.github.com>
2014-03-20 13:37:17 +0100

    Update getGroupMembers.ps1

--- getGroupMembers.ps1
+++ getGroupMembers.ps1
@@ -10,17 +10,26 @@ else
 {
 	$file_name = $(".\" + $group + "_members.txt")
 }
-$users = Get-ADGroupMember -I $group | sort Name | Format-List SamAccountName | Out-File $file_name
-#Remove empty lines and
+$temporaryFile = ".\temp.txt"
+if (Test-Path $temporaryFile)
+{
+	Remove-Item $temporaryFile
+}
+$users = Get-ADGroupMember -I $group | sort Name | Format-List SamAccountName | Out-File $temporaryFile #$file_name
+#Remove empty lines and \\
 #Keep only the last word of the "Name : " line (where the userID is)
-(gc $file_name) |? {$_.trim() -ne  "" }| %{$_.Split()[-1]} | Set-Content $file_name
+#(gc $file_name) |? {$_.trim() -ne  "" }| %{$_.Split()[-1]} | Set-Content $file_name
+(gc $temporaryFile) |? {$_.trim() -ne  "" }| %{$_.Split()[-1]} | Set-Content $temporaryFile
 #Get the name from the given UserIDs
-$members = Get-Content $file_name
+$members = Get-Content $temporaryFile
+if ($isNested -ne "yes")
+{
+	Remove-Item $file_name
+}
 foreach ($member in $members)
 {
-	if($member.StartsWith("IR_"))
+	if($member.ToLower().StartsWith("cs_") -or $member.ToLower().StartsWith("ir_") -or $member.ToLower().StartsWith("lr_") -or $member.ToLower().StartsWith("mbna_") -or $member.ToLower().StartsWith("mm") -or $member.ToLower().StartsWith("rs_") -or $member.ToLower().StartsWith("sccm") -or $member.ToLower().StartsWith("sp_") or $member.ToLower().StartsWith("dg"))
 	{
-		#
 		if ($isNested -eq "yes")
 		{
 			.\getGroupMembers.ps1 -group $member -isNested yes -nestedFrom $nestedFrom
@@ -35,4 +44,8 @@ foreach ($member in $members)
 		Get-ADUser -I $member | Format-Table -HideTableHeaders UserPrincipalName, Surname, GivenName | Out-File -encoding ascii -Append $file_name
 	}
 }
-(gc $file_name) | ? {$_.trim() -ne  "" } | Set-Content $file_name
+if (Test-Path $temporaryFile)
+{
+	Remove-Item $temporaryFile
+}
+(gc $file_name) | ? {$_.trim() -ne  "" } | sort -Unique | Set-Content $file_name
