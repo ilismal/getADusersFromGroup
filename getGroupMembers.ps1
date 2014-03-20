b04c35c940a8d16198b6645393859233e8ca0537
Ignacio Lis <ilismal@users.noreply.github.com>
2014-03-20 14:49:37 +0100

    Update getGroupMembers.ps1

--- getGroupMembers.ps1
+++ getGroupMembers.ps1
@@ -2,6 +2,7 @@ Param ([string]$group = $(throw "-group is required"), $isNested, $nestedFrom)
 Import-Module ActiveDirectory
 
 #We'll export the output to a text file named after the target group
+#If the script is casted with the -isNested flag, we know that we are dealing with a group within a group
 if ($isNested -eq "yes")
 {
 	$file_name = $(".\" + $nestedFrom + "_members.txt")
@@ -11,24 +12,28 @@ else
 	$file_name = $(".\" + $group + "_members.txt")
 }
 $temporaryFile = ".\temp.txt"
+#We get rid of previous temporary files, if any
 if (Test-Path $temporaryFile)
 {
 	Remove-Item $temporaryFile
 }
+#Get the UserIDs that belong to the target group
 $users = Get-ADGroupMember -I $group | sort Name | Format-List SamAccountName | Out-File $temporaryFile #$file_name
 #Remove empty lines and \\
-#Keep only the last word of the "Name : " line (where the userID is)
-#(gc $file_name) |? {$_.trim() -ne  "" }| %{$_.Split()[-1]} | Set-Content $file_name
+#Keep only the last word of the line (where the userID is)
 (gc $temporaryFile) |? {$_.trim() -ne  "" }| %{$_.Split()[-1]} | Set-Content $temporaryFile
 #Get the name from the given UserIDs
 $members = Get-Content $temporaryFile
+#Remove previous files just in the first iteration
 if ($isNested -ne "yes")
 {
 	Remove-Item $file_name
 }
 foreach ($member in $members)
 {
-	if($member.ToLower().StartsWith("cs_") -or $member.ToLower().StartsWith("ir_") -or $member.ToLower().StartsWith("lr_") -or $member.ToLower().StartsWith("mbna_") -or $member.ToLower().StartsWith("mm") -or $member.ToLower().StartsWith("rs_") -or $member.ToLower().StartsWith("sccm") -or $member.ToLower().StartsWith("sp_") or $member.ToLower().StartsWith("dg"))
+	#In our environment usernames are seven chars long, bigger members would be groups
+	#Let's use some recursion here...
+	if ($member.length -gt 7)
 	{
 		if ($isNested -eq "yes")
 		{
@@ -44,8 +49,10 @@ foreach ($member in $members)
 		Get-ADUser -I $member | Format-Table -HideTableHeaders UserPrincipalName, Surname, GivenName | Out-File -encoding ascii -Append $file_name
 	}
 }
+#Remove temporary files
 if (Test-Path $temporaryFile)
 {
 	Remove-Item $temporaryFile
 }
+#Remove duplicate lines
 (gc $file_name) | ? {$_.trim() -ne  "" } | sort -Unique | Set-Content $file_name
